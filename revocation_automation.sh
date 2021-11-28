62945b9761d236015915eab99c0343ee1885d44e
Bisakha Das <56753634+BisakhaD@users.noreply.github.com>
2021-11-28 11:14:41 +0800

    Update revocation_automation.sh

--- revocation_automation.sh
+++ revocation_automation.sh
@@ -5,12 +5,12 @@ openssl ca -config intermediate/openssl.cnf -gencrl -out intermediate/crl/interm
 openssl crl -in intermediate/crl/intermediate.crl.pem -noout -text
 
 #mv bob/www.${rev_name}.csr.pem intermediate/csr/www.${rev_name}.csr.pem
-
-openssl ca -config intermediate/openssl.cnf -extensions usr_cert -notext -md sha256 -in intermediate/csr/www.${rev_name}.csr.pem -out intermediate/certs/www.${rev_name}.cert.pem
-
+echo
+#openssl ca -config intermediate/openssl.cnf -extensions usr_cert -notext -md sha256 -in intermediate/csr/www.${rev_name}.csr.pem -out intermediate/certs/www.${rev_name}.cert.pem
+echo
 openssl verify -CAfile intermediate/certs/ca-chain.cert.pem intermediate/certs/www.${rev_name}.cert.pem
-
+echo
 openssl ca -config intermediate/openssl.cnf -revoke intermediate/certs/www.${rev_name}.cert.pem
-
+echo
 # Recreating the crl after revoking bob
 openssl ca -config intermediate/openssl.cnf -gencrl -out intermediate/crl/intermediate.crl.pem
